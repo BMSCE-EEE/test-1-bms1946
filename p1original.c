10504f430abac41df16fdfe9a253c67119c07c9b
RAVICHANDRA BADIGER <95006866+bms1946@users.noreply.github.com>
2021-11-26 03:43:10 +0000

    second one

--- p1original.c
+++ p1original.c
@@ -1,2 +1,30 @@
-
+#include<stdio.h>
+float add(float x,float y);
+float input();
+void output(float add);
+int main()
+{
+  float a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(sum);
+  return 0;
+}
+float input()
+{
+  float A;
+  printf("enter anumber\n");
+  scanf("%f",&A);
+  return A;
+}
+float add(float x,float y)
+{
+  float s;
+  s=x+y;
+  return s;
+}
+void output(float add)
+{
+  printf("the sum of the two number :%0.2f",add);
 }
